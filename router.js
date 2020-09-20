5d07bfbdeb92745b3c34f7c6cb80812c760ebf00
demid-dk <71603634+demid-dk@users.noreply.github.com>
2020-09-20 22:02:28 +0400

    Update router.js

--- router.js
+++ router.js
@@ -1,12 +1,10 @@
-ymaps.ready(init);
-
-function init() {
-    var myMap = new ymaps.Map("map", {
-            center: [55.745508, 37.435225],
-            zoom: 13
-            controls: ['routeButtonControl']
-       
-        });
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+        center: [55.753994, 37.622093],
+        zoom: 9,
+        // Добавим кнопку для построения маршрутов на карту.
+        controls: ['routeButtonControl']
+    });
 
     var control = myMap.controls.get('routeButtonControl');
     
