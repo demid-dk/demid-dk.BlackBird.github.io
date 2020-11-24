273a4f1dbc46bb852f9d48cb1c577df7df32ee25
demid-dk <71603634+demid-dk@users.noreply.github.com>
2020-11-24 11:45:07 +0400

    Add files via upload

---
+++ ss.js
@@ -0,0 +1,30 @@
+$(document).ready(()=>{
+site()
+function site(){
+    $('#img img').outerHeight($('#header').height()-40)
+    $('#ytFrame').outerHeight(window.innerHeight - window.innerHeight*0.3)
+    $('#ytFrame').outerWidth((window.innerWidth-10)-(window.innerWidth-10)*0.3)
+}
+
+$("#theme").click(()=>{
+    $("#custom").toggleClass("show")
+})
+$("#l1").click(()=>{
+    $(document.body.children).toggleClass("st1")
+    $(document.body.children).toggleClass("l1")
+})
+})
+// function resizeIframe(obj) {
+//     if (obj.contentWindow.document.body) {
+//         if (obj.clientHeight < obj.contentWindow.document.body.scrollHeight) {
+//             $(obj).animate({ 'height': obj.contentWindow.document.body.scrollHeight + 10 });
+//             setTimeout(function () { resizeIframeTuning(obj) }, 400);
+//         }
+//     }
+// }
+// function resizeIframeTuning(obj) {
+//     if (obj && obj.contentWindow && obj.contentWindow.document && obj.contentWindow.document.body.clientWidth < obj.contentWindow.innerWidth) {
+//         $(obj).animate({ 'height': obj.clientHeight + 20 }, 100);
+//         setTimeout(function () { resizeIframeTuning(obj) }, 100);
+//     }
+// }
