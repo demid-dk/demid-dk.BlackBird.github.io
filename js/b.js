72e39175413d8797740c897aea27425a49d6c3e2
demid-dk <71603634+demid-dk@users.noreply.github.com>
2021-10-27 11:39:01 +0300

    Add files via upload

--- js/b.js
+++ js/b.js
@@ -12,3 +12,12 @@ $("#x").click(()=>{
     $("header").toggleClass("f34")
     $("header").toggleClass("c22")
 })
+$(window).resize(function () {
+    var width = window.innerWidth
+    if (width <= 768) {
+        $('.content_con').removeClass('con_row').addClass('con_col');
+    }
+    else{
+        $('.content_con').addClass('con_row').removeClass('con_col');
+    }
+});
