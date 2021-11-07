a26d5462accd02dc8b965f2b94d70870b5f00704
demid-dk <71603634+demid-dk@users.noreply.github.com>
2021-11-07 21:57:45 +0300

    Add files via upload

--- b.js
+++ b.js
@@ -15,10 +15,28 @@ $("#x").click(()=>{
 function widthResize(){
     var width = window.innerWidth
     if (width <= 768) {
-        $('.content_con').removeClass('con_row').addClass('con_col');
+        $('#fr1_cc').removeClass('con_row').addClass('con_col');
     }
     else{
-        $('.content_con').addClass('con_row').removeClass('con_col');
+        $('#fr1_cc').addClass('con_row').removeClass('con_col');
+    }
+    if (width <= 1000) {
+        $('#fr3_cc').addClass('con_row').removeClass('con_col');
+        $('#fr4_cc').addClass('con_row').removeClass('con_col');
+    }
+    else{
+        $('#fr3_cc').removeClass('con_row').addClass('con_col');
+        $('#fr4_cc').removeClass('con_row').addClass('con_col');
+    }
+    if (width <= 600) {
+        $('#fr7_cc').removeClass('con_row').addClass('con_col');
+        $('#fr8_cc').removeClass('con_row').addClass('con_col');
+        $('#fr9_cc').removeClass('con_row').addClass('con_col');
+    }
+    else{
+        $('#fr7_cc').removeClass('con_col').addClass('con_row');
+        $('#fr8_cc').removeClass('con_col').addClass('con_row');
+        $('#fr9_cc').removeClass('con_col').addClass('con_row');
     }
 }
 setInterval(widthResize, 1000/60)
